994378aecb03f7c9f792c6346b6d533c4fdf3e0b
Quentin Devos <uw2k7_saotome@live.be>
2014-06-23 12:17:01 +0000

    token.php edited online with Bitbucket

--- token.php
+++ token.php
@@ -20,5 +20,6 @@ require_once __DIR__ . '/../../claroline/inc/claro_init_global.inc.php';
 require_once __DIR__ . '/lib/InitServer.php';
 
 // Handle a request for an OAuth2.0 Access Token and send the response to the client
-// Transform the Authorization Token in
+// Transform the Authorization Token in an Access Token valid for 3600 sec by default
+// and a Refresh Token to use to get new Access Tokens when the previous are expired.
 $server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
